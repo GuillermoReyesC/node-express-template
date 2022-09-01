740a968c07b21a8351a9fe46f65a74df8093adb7
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-01 15:17:10 -0400

    server

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ const app = require("src/app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server listening in port https://localhost/${PORT}`);
+  console.log(`Server listening in port https://localhost:${PORT}`);
 });
