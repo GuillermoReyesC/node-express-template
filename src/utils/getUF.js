ad50656d2b8e82b67b064091c992572708ea6a20
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-02 14:31:42 -0400

    set time out to getUF

--- src/utils/getUF.js
+++ src/utils/getUF.js
@@ -2,7 +2,7 @@ const axios = require('axios')
 
 const getUF = async () => {
   const UF = await axios
-    .get(process.env.API_DIVISAS_UF)
+    .get(process.env.API_DIVISAS_UF, { timeout: 3000 })
     .then((data) => {
       return data.uf.valor
     })
