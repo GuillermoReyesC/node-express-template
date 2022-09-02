7f421b51b26cbc81e61ba213bbbef2f7c9d1131e
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-02 03:32:13 -0400

    uf & credit controllers

--- src/controllers/badges/retrieveUF.controller.js
+++ src/controllers/badges/retrieveUF.controller.js
@@ -3,11 +3,11 @@ const { ApiError } = require('src/errors')
 
 const { getUF } = require('src/utils')
 
-const retrieveUF = (req, res, next) => {
+const retrieveUF = async (req, res, next) => {
   try {
-    const { quantity } = req.params
-    const UF = getUF()
-    const result = quantity / UF ;
+    const { amount } = req.params
+    const UF = await getUF()
+    const result = amount / UF ;
     res.status(200).json({ UF: result })
   } catch (error) {
     next(ApiError.badRequest(error.message))
--- src/controllers/simulator/creditRequest.controller.js
+++ src/controllers/simulator/creditRequest.controller.js
@@ -1,10 +1,31 @@
 const { ApiError } = require('src/errors')
 
-const creditRequest = (req, res, next) => {
+const { User, CreditRequest } = require('src/models')
+const { formatCLP, paymentPlan } = require('src/utils')
+const { getUF } = require('src/utils')
+
+const creditRequest = async (req, res, next) => {
   try {
-    res.status(200).json(req.body)
+    const { uf, dues, date } = req.body
+    const ufValue = await getUF()
+    const [user, _] = await User.findOrCreate({
+      where: { ...req.body.user },
+    })
+    const creditCreated = await CreditRequest.create({
+      fk_user_id: user.id,
+      uf: uf,
+      dues: dues,
+      date: date,
+    })
+    res.status(200).json({
+      identifier: creditCreated.id,
+      uf: uf,
+      clp: formatCLP(ufValue * uf),
+      duesValue: formatCLP((ufValue * uf) / dues),
+      payment_plan: paymentPlan(date, dues)
+    })
   } catch (error) {
-    next(ApiError.badRequest('algo malo paso'))
+    next(ApiError.badRequest(error))
   }
 }
 
