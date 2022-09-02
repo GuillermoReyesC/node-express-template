205219a20f74c15f26fa38ca8b5019ca23705665
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-02 14:49:16 -0400

    routes

--- src/routes/simulator.routes.js
+++ src/routes/simulator.routes.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 
 const { validateSchema } = require("src/middlewares");
-const { creditSchema } = require("src/schemas");
+const { creditSchema, updateCreditSchema } = require("src/schemas");
 
-const { creditRequest } = require("src/controllers/simulator");
+const { creditRequest, getCredit, deleteCredit, updateCredit } = require("src/controllers/simulator");
 
 router.post("/credit", validateSchema(creditSchema) ,creditRequest);
+router.get("/credit/:id", getCredit);
+router.delete("/credit/:id", deleteCredit);
+router.put("/credit/:id", validateSchema(updateCreditSchema), updateCredit);
+
 
 module.exports = router;
