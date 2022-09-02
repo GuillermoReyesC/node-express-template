dc4ee531852301941d6d28931f2533ce70683c27
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-02 19:37:56 -0400

    dayjs

--- src/utils/paymentPlan.js
+++ src/utils/paymentPlan.js
@@ -4,7 +4,7 @@ const paymentPlan = (date, dues) => {
     const dates = []
     const now = dayjs(date);
     for(i=1; i < dues; i++) {
-        dates.push(now.month(now.month() + i).date(5).hour(0).minute(0).second(0))
+        dates.push(dayjs(now.month(now.month() + i).date(5).hour(0).minute(0).second(0)).format("YYYY-MM-DD"));
     }
     return dates
 }
