6b7f1629c63f755b7f6b5f1a2e1e75599f537f21
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-01 22:41:39 -0400

    uf controller

--- src/controllers/badges/retrieveUF.controller.js
+++ src/controllers/badges/retrieveUF.controller.js
@@ -7,7 +7,7 @@ const retrieveUF = (req, res, next) => {
   try {
     const { quantity } = req.params
     const UF = getUF()
-    const result = quantity / UF * 100 ;
+    const result = quantity / UF ;
     res.status(200).json({ UF: result })
   } catch (error) {
     next(ApiError.badRequest(error.message))
