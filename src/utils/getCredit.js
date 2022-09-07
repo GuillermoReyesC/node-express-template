601335a83c25ef85391a37d684ab7ee738c1af9a
Guillermo Reyes Campos <guillermo.a.reyes.c@gmail.com>
2022-09-07 16:33:41 -0300

    unused files

--- src/utils/getCredit.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios');
-
-const getCredit = async () => {
-    const request = await axios.get(process.env.API_CREDIT_URL);
-    return request.data;
-}
-
-module.exports = getCredit;
